b7389464f1da0b1d43317f5149dbbec4e8aeae83
Noah McCampbell <57782774+noahmccampbell@users.noreply.github.com>
2025-03-01 16:02:08 -0500

    Fixed Styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage/>} />
         <Route path="log-in" element={<Login />} />
       </Routes>
     </Router>
