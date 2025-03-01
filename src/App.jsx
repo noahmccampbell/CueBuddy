9b14a419a50cb53c87c22b8d9706e9d4a7bdf7fd
Noah McCampbell <57782774+noahmccampbell@users.noreply.github.com>
2025-03-01 17:52:14 -0500

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from './pages/LandingPage'
-import Login from './pages/Login'
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './pages/Login'
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="log-in" element={<Login />} />
+        <Route path="/" element={<Login/>} />
+        <Route path="/landing" element={<LandingPage/>} />
       </Routes>
     </Router>
   )
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,8 +9,8 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${FIREBASE_API_KEY}`,
-  authDomain: `${FIREBASE_AUTH_DOMAIN}`,
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
   projectId: "cuebuddy-56563",
   storageBucket: "cuebuddy-56563.firebasestorage.app",
   messagingSenderId: "437091310726",
@@ -18,5 +19,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const fire = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
